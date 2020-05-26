5e5e5c2f52c16deba1e6fbf4b1d4ed50ce27ded0
Reza Hoque <Reza.Hoque@outlook.com>
2020-05-26 16:18:57 +0200

    class

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 #https://code.visualstudio.com/docs/python/python-tutorial
 #https://pythonprogramming.net/machine-learning-tutorials/
 
-msg="Hello python"
-print(msg)
+class hello:
+    def __init__(self,name,age):
+        self.n=name
+        self.a=age
+
+x=hello("kabbo",7)
+print(x.n + " is " + str(x.a) + " now")
+
 
